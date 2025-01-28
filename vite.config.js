c3d09aea8a280e5bb7767a6602f0a183f5e4fe49
Doga <lindaltelrunya@hotmail.com>
2025-01-28 12:17:54 +0300

    chore: transfre SCSS files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,39 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import inject from "@rollup/plugin-inject";
 
-export default defineConfig({
+var config = {
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/scss/style.scss',    // Add both entry points
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
+        inject({
+            $: 'jquery',
+            jQuery: 'jquery',
+        })
     ],
+    server: {
+        origin: 'http://localhost:5173',
+        cors: true
+    },
+    build: {
+        assetsDir: '',
+    },
+};
+
+export default defineConfig(({command, mode, ssrBuild}) => {
+    if (command === 'serve') {
+        config.publicDir = 'public';
+        config.build = {
+            assetsDir: '',
+            copyPublicDir: false,
+            emptyOutDir: true,
+        };
+    }
+
+    return config;
 });
