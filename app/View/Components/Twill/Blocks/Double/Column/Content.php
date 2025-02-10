445f01096f0219d9e5b77932a3e4b80683f4ebd6
ramazan <ramazan.ayyildiz@gmail.com>
2025-02-10 14:30:45 +0300

    create Double Column Content Block  and add twill-image package and update dependencies

---
+++ app/View/Components/Twill/Blocks/Double/Column/Content.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\View\Components\Twill\Blocks\Double\Column;
+
+use A17\Twill\Models\Block;
+use A17\Twill\Services\Forms\Fields\Checkbox;
+use A17\Twill\Services\Forms\Fields\Input;
+use A17\Twill\Services\Forms\Fields\Medias;
+use A17\Twill\Services\Forms\Fields\Radios;
+use A17\Twill\Services\Forms\Fields\Wysiwyg;
+use A17\Twill\Services\Forms\Form;
+use A17\Twill\Services\Forms\Option;
+use A17\Twill\Services\Forms\Options;
+use A17\Twill\View\Components\Blocks\TwillBlockComponent;
+use Illuminate\Contracts\View\View;
+
+class Content extends TwillBlockComponent
+{
+    public function render(): View
+    {
+        return view('components.twill.blocks.double.column.content');
+    }
+
+    public function getForm(): Form
+    {
+        return Form::make([
+            Checkbox::make()
+                ->name('image_right')
+                ->label('Right Image Position'),
+            Checkbox::make()
+                ->name('reverse_order_mobile')
+                ->label('Reverse Order on Mobile'),
+            Radios::make()
+                ->name('color_scheme')
+                ->inline()
+                ->border()
+                ->options(
+                    Options::make([
+                        Option::make('green', 'Green'),
+                        Option::make('peach', 'Peach'),
+                    ])
+                )->default('green'),
+            Wysiwyg::make()->name('text')->label('Content')->required()->translatable()
+                ->toolbarOptions([
+                    ['header' => [2, 3, false]],
+                    'strong',
+                    'italic',
+                    'underline',
+                    'link'
+                ])->allowSource(true),
+            Medias::make()
+                ->name('col_image')
+                ->label('Image')
+                ->required()
+                ->max(1)
+        ]);
+    }
+
+    public static function getCrops(): array
+    {
+        return [
+            'col_image' => [
+                'default' => [
+                    [
+                        'name' => 'default',
+                        'ratio' => 0,
+                        'minValues' => [
+                            'width' => 643,
+                            'height' => 466,
+                        ],
+                    ],
+                ]
+            ]
+        ];
+    }
+
+    public static function getBlockTitle(?Block $block = null): string
+    {
+        return "Double Column Content";
+    }
+}
---
+++ config/twill-image.php
@@ -0,0 +1,140 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Low Quality Placeholder
+    |--------------------------------------------------------------------------
+    |
+    | Tells if LQIP should be used if it is available.
+    |
+    */
+    'lqip' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Use image sizer element
+    |--------------------------------------------------------------------------
+    |
+    | If sets to auto, the sizer will be used if LQIP is enabled.
+    | It can be set to `true` or `false` to enable or disable.
+    |
+    */
+    'image_sizer' => 'auto',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable WebP Support
+    |--------------------------------------------------------------------------
+    |
+    | Add sources support for WepP images.
+    |
+    */
+    'webp_support' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mode
+    |--------------------------------------------------------------------------
+    |
+    | Use inline styles for default styling or use classes instead.
+    |
+    | In the example below, classes are used for applying
+    | Tailwind CSS classes.
+    |
+    */
+    'mode' => 'inline-styles', // 'inline-styles' | 'classes' | 'both'
+
+    'inline_styles' => false,
+
+    'classes' => [
+        'main' => [
+            'h-full',
+            'm-0',
+            'max-w-none',
+            'p-0',
+            'w-full',
+            'object-cover',
+            'object-center'
+        ],
+        'wrapper' => [
+            'relative',
+            'overflow-hidden',
+        ],
+        'placeholder' => [
+            'absolute',
+            'inset-0',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Image Presets
+    |--------------------------------------------------------------------------
+    |
+    | Define image presets here.
+    |
+    */
+    'presets' => [
+        // Preset example
+        // 'preview_image' => [
+        //     'crop' => 'default',
+        //     'sizes' => '25vw',
+        // ],
+
+        // Preset example with multiple crops
+        // 'art_directed' => [
+        //     'crop' => 'desktop',
+        //     'width' => 700,
+        //     'sizes' => '(max-width: 767px) 100vw, (min-width: 767px) and (max-width: 1023px) 50vw, 33vw',
+        //     'sources' => [
+        //         [
+        //             'crop' => 'mobile',
+        //             'media_query' => '(max-width: 767px)',
+        //         ],
+        //         [
+        //             'crop' => 'tablet',
+        //             'media_query' => '(min-width: 767px) and (max-width: 1023px)',
+        //         ],
+        //         [
+        //             'crop' => 'desktop',
+        //             'media_query' => '(min-width: 1024px)',
+        //         ]
+        //         // ...
+        //     ],
+        // ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Columns - Frontend breakpoints and grid structure
+    |--------------------------------------------------------------------------
+    |
+    | Define the columns class that is used to dynamically generates
+    | `sizes` and `media`.
+    |
+    */
+    'columns_class' => A17\Twill\Image\Services\ImageColumns::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Static Images Local Path
+    |--------------------------------------------------------------------------
+    |
+    | Define the local path where the static images
+    | are located. This should correcponds to the Twill `ImageService`
+    | source folder and be publicly available.
+    |
+    */
+    'static_local_path' => public_path(),
+
+    'static_image_support' => false,
+
+    // Glide config overrides
+    'glide' => [
+        'source' => public_path(),
+        'base_path' => 'static',
+    ],
+
+];
