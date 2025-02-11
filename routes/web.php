7dade5b8c72b27b79ef337babb5cade4e766179d
Doga <lindaltelrunya@hotmail.com>
2025-02-11 13:32:59 +0300

    chore: change order of the routes

--- routes/web.php
+++ routes/web.php
@@ -11,33 +11,6 @@ use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
-// Route::controller(FormController::class)->group(function () {
-//     Route::post('/submit-request', 'submitRequestForm')->name('submit-request');
-//     Route::post('/submit-newsletter', 'newsletter')->name('submit-newsletter');
-
-// });
-// Route::get('sitemap.xml', [SitemapController::class, 'sitemap']);
-Route::group([
-    'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => [
-        //!TODO: 'redirect',
-        //!TODO: 'noIndexNoFollow',
-        'localize',  'localizationRedirect', 'localeViewPath'],
-], function () {
-
-    Route::get(LaravelLocalization::transRoute('routes.home'), [PageHomeDisplayController::class, 'show'])->name('home');
-    Route::get('/', [\App\Http\Controllers\PageHomeDisplayController::class, 'show'])->name('frontend.home');
-    Route::get(LaravelLocalization::transRoute('routes.page'), [PageDisplayController::class, 'show'])->name('page');
-    // Route::get(LaravelLocalization::transRoute('routes.success'), [PageDisplayController::class, 'success'])->name('success');
-    // Route::get(LaravelLocalization::transRoute('routes.articles'), [ArticleDisplayController::class, 'index'])->name('articles');
-    // Route::get(LaravelLocalization::transRoute('routes.article'), [ArticleDisplayController::class, 'show'])->name('article');
-    // Route::get(LaravelLocalization::transRoute('routes.page'), [PageDisplayController::class, 'show'])->name('page');
-    // Route::get(LaravelLocalization::transRoute('routes.companyEstablishment'), [CompanyEstablishmentDisplayController::class, 'show'])->name('companyEstablishment');
-    // Route::get(LaravelLocalization::transRoute('routes.employerService'), [EmployerServiceDisplayController::class, 'show'])->name('employerService');
-    // Route::get(LaravelLocalization::transRoute('routes.success'), [PageDisplayController::class, 'success'])->name('success');
-});
-
-
 Route::get('/for-all-beings', function () {
     return view('for-all-beings');
 });
@@ -58,3 +31,30 @@ Route::get('/packaging', function () {
 Route::get('/shipping', function () {
     return view('shipping');
 });
+// Route::controller(FormController::class)->group(function () {
+//     Route::post('/submit-request', 'submitRequestForm')->name('submit-request');
+//     Route::post('/submit-newsletter', 'newsletter')->name('submit-newsletter');
+
+// });
+// Route::get('sitemap.xml', [SitemapController::class, 'sitemap']);
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => [
+        //!TODO: 'redirect',
+        //!TODO: 'noIndexNoFollow',
+        'localize',  'localizationRedirect', 'localeViewPath'],
+], function () {
+
+    // Route::get(LaravelLocalization::transRoute('routes.home'), [PageHomeDisplayController::class, 'show'])->name('home');
+    // Route::get('/', [\App\Http\Controllers\PageHomeDisplayController::class, 'show'])->name('frontend.home');
+    Route::get(LaravelLocalization::transRoute('routes.page'), [PageDisplayController::class, 'show'])->name('page');
+    // Route::get(LaravelLocalization::transRoute('routes.success'), [PageDisplayController::class, 'success'])->name('success');
+    // Route::get(LaravelLocalization::transRoute('routes.articles'), [ArticleDisplayController::class, 'index'])->name('articles');
+    // Route::get(LaravelLocalization::transRoute('routes.article'), [ArticleDisplayController::class, 'show'])->name('article');
+    // Route::get(LaravelLocalization::transRoute('routes.page'), [PageDisplayController::class, 'show'])->name('page');
+    // Route::get(LaravelLocalization::transRoute('routes.companyEstablishment'), [CompanyEstablishmentDisplayController::class, 'show'])->name('companyEstablishment');
+    // Route::get(LaravelLocalization::transRoute('routes.employerService'), [EmployerServiceDisplayController::class, 'show'])->name('employerService');
+    // Route::get(LaravelLocalization::transRoute('routes.success'), [PageDisplayController::class, 'success'])->name('success');
+});
+
+
