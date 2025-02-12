9ab3dcdffa72e5b5c1f1fe8b9412610bf0e49956
ramazan <ramazan.ayyildiz@gmail.com>
2025-02-12 11:58:27 +0300

    Doublecrossedcolumnswithcontent block added

---
+++ resources/views/components/twill/blocks/doublecrossedcolumnswithcontent.blade.php
@@ -0,0 +1,12 @@
+<section class="double-crossed-colums-with-content">
+  <div class="container xlarge">
+      <div class="row gap-30">
+          <div class="col-md-6 left-bubble">
+              {!! $translatedInput('text_left') !!}
+          </div>
+          <div class="col-md-7 right-bubble">
+            {!! $translatedInput('text_right') !!} 
+          </div>
+      </div>
+  </div>
+</section>
