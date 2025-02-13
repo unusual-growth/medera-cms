0da0caf025608ee0ed1c8aad892a46b0777c77ce
ramazan <ramazan.ayyildiz@gmail.com>
2025-02-13 16:15:47 +0300

    refactor: Simplify article hero section markup and structure

--- resources/views/site/article.blade.php
+++ resources/views/site/article.blade.php
@@ -4,31 +4,19 @@
         // dd( $item);
 
     @endphp
-    <section class="article-hero color-white font-size-16" itemscope itemtype="https://schema.org/Article">
-        @php
-            // $image = TwillImage::make($item, 'library-image')->crop('default')->width(2880)->height(800);
-            //  $image["image"]["itemprop"]="image";
-            // dd($image)
-            // dd($item->image('library-image','default'));
-        @endphp
-        {{-- {!! TwillImage::render($image) !!} --}}
-        <picture>
-            <img src="{{ $item->image('library-image','default') }}" alt="{{ $item->title }}" itemprop="image">
-        </picture>
 
-        <div class="container medium text-align-center">
-
-            <p class="text-align-center">
-                <span class="date text-align-center" itemprop="datePublished" content="{{\Carbon\Carbon::parse($item->publish_start_date)}}">
-                    {{ \Carbon\Carbon::parse($item->publish_start_date)->translatedFormat('d M Y') }}
-                </span>
-            </p>
-            <h1 itemprop="headline">
+<section class="article-hero color-white font-size-16" itemscope itemtype="https://schema.org/Article">
+    <div class="container">
+        <div class="library-content">
+            <span itemprop="datePublished" content="{{\Carbon\Carbon::parse($item->publish_start_date)->translatedFormat('d M Y')}}">
+                {{ \Carbon\Carbon::parse($item->publish_start_date)->translatedFormat('d M Y') }}
+            </span>
+            <h1>
                 {{ $item->title }}
             </h1>
-            <p></p>
         </div>
-    </section>
+    </div>
+</section>
     <section class="article-body no-padding">
         {!! $item->renderBlocks() !!}
     </section>
