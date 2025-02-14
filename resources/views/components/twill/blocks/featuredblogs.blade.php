029bd45ce017dc1aff1551afc71761c5824332c4
Doga <lindaltelrunya@hotmail.com>
2025-02-14 15:00:38 +0300

    feat: add 3 block, fix some stuff

---
+++ resources/views/components/twill/blocks/featuredblogs.blade.php
@@ -0,0 +1,17 @@
+<section class="background-primary-lighten-4">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="sect-heading-desc col-md-9 flex-column align-items-center">
+                <h2 class="text-align-center color-secondary">{{__('util.other-blogs')}}</h1>
+                <p class="text-align-center">{{__('util.other-blogs-description')}}</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-display library">
+        {{-- !!TODO: put inside a slider for mobile --}}
+        @foreach ($blogs as $relatedArticle)
+            <x-article-card :article="$relatedArticle" />
+        @endforeach
+    </div>
+</section>
