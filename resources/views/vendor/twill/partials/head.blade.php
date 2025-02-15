a5be7efdd89c50d40c03dcdf4290552897e52bef
ramazan <ramazan.ayyildiz@gmail.com>
2025-02-15 22:13:51 +0300

    style: Customize Twill admin panel UI with new color scheme and styling

--- resources/views/vendor/twill/partials/head.blade.php
+++ resources/views/vendor/twill/partials/head.blade.php
@@ -30,6 +30,9 @@
 @stack('extra_css')
 @stack('extra_js_head')
 <style>
+    body{
+        background-color: #e9e9e9;
+    }
    .twill-layout {
     display: flex;
     min-height: 100vh;
@@ -38,10 +41,26 @@
 .twill-sidebar-menu {
     width: 250px;
     min-width: 250px;
-    background-color: #1a1a1a;
+    background-color: #fdfdfd;
     padding: 20px;
     height: auto;
     overflow-y: auto;
+    -webkit-box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+    -moz-box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+    -ms-box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+    z-index: 11 !important;
+}
+
+.header__item.s--on, .header__item:hover{
+    background-color: transparent !important;
+}
+
+.header__item.s--on a, .header__item:hover a{
+    color: #f44336;
+}
+.header__item {
+    color:#333;
 }
 @media screen and (max-width: 1024px) {
     .twill-sidebar-menu {
@@ -89,6 +108,19 @@
 .header__item {
     width: 100%;
     margin: 0 !important;
+    position: relative;
+    border-bottom: 1px solid #eee;
+}
+
+.header__item::before {
+    content: "►";  /* You can change this symbol to anything you want */
+    position: absolute;
+    left: 0px;
+    top: 50%;
+    transform: translateY(-50%);
+    color: #b4b4b4;
+    font-size: 8px;
+    margin-left: 5px;
 }
 
 .header__subitem {
@@ -120,5 +152,102 @@
     }
 }
 
+header.header {
+    background: #f44336;
+    -webkit-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+    -ms-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+    padding: 5px 0;
+}
+.header__title{
+    font-size: 18px;
+}
 
+.dashboardSearch {
+    background: #f44336;
+    -webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+    -ms-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+}
+
+.container.search.search--dashboard {
+    background: #f44336;
+}
+
+.search--dashboard .search__input .form__input{
+    background: #fff !important;
+}
+
+
+
+.headerSearch__toggle .icon{color: #fff;}
+.headerSearch__toggle .icon:hover{color: #ccc;}
+
+.listing,#content,.fieldset,.publisher__wrapper{
+    background: #fff;
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2);
+}
+
+.fieldset__header{
+    background: #ffc008 !important;
+    color: #fff;
+}
+.block__header{
+ background: #607D8B    !important;
+ color: #fff !important;
+ }
+
+ .block--small .block__header{
+    background: #9baeb7    !important;
+    color: #fff !important;
+ }
+
+ .block__handle:before{
+    background: repeating-linear-gradient(180deg, #9baeb7, #9baeb7 2px, transparent 0, transparent 4px) !important;
+}
+
+.a17.a17--login {
+    background: #00416a; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #6496b6, #e4e5e6); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #6496b6, #e4e5e6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+//background: linear-gradient(110deg, #fdcd3b 60%, #ffed4b 60%);
+}
+
+.a17.a17--login form{
+background:rgba(255,255,255,0.5);
+    padding:30px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+}
+
+.login__heading{
+    color: #00416a;
+    margin:-30px -30px 0 -30px;
+    background:rgba(255,255,255,0.5);
+    padding:30px;    text-align: center;
+}
+h2.login__heading{
+    display: none;
+}
+.login__input {
+    height: 34px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.42857143;
+    color: #555;
+    border: 0;
+    border-bottom: 1px solid #555;
+    background-color: transparent !important;
+}
+
+.login__button{
+    background-color: #E91E63 !important;
+}
+input:-webkit-autofill,
+  input:-webkit-autofill:focus {
+    transition: background-color 0s 600000s, color 0s 600000s !important;
+  }
+.footer{display: none;}
+.login__copyright {display: none;}
 </style>
