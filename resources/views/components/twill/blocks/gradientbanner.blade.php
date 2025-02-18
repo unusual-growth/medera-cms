277ebbedc95d780465e3fc122201e333d868b2ae
Doga <lindaltelrunya@hotmail.com>
2025-02-18 10:26:48 +0300

    fix:

--- resources/views/components/twill/blocks/gradientbanner.blade.php
+++ resources/views/components/twill/blocks/gradientbanner.blade.php
@@ -3,10 +3,10 @@
         <div class="row justify-content-center">
             <div class="col-md-6 flex-column align-items-center">
                 <div class="col-md-12">
-                    <h1 class="text-align-center">{{$input('title')}}</h1>
+                    <h1 class="text-align-center">{{$translatedInput('title')}}</h1>
                 </div>
                 <div class=" col-md-10 col-lg-8">
-                    <p class="text-align-center">{{$input('text')}}</p>
+                    <p class="text-align-center">{{$translatedInput('text')}}</p>
                 </div>
             </div>
         </div>
