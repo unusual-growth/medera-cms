ccfb59095d09ddc7a00f4d4d592f22ecc0d75224
Doga <lindaltelrunya@hotmail.com>
2025-02-23 22:41:06 +0300

    feat: add inline banner with glass background

--- app/View/Components/Twill/Blocks/Inlinebannerwithglasscontainer.php
+++ app/View/Components/Twill/Blocks/Inlinebannerwithglasscontainer.php
@@ -2,10 +2,14 @@
 
 namespace App\View\Components\Twill\Blocks;
 
+use A17\Twill\Models\Block;
 use A17\Twill\Services\Forms\Fields\Wysiwyg;
 use A17\Twill\Services\Forms\Form;
 use A17\Twill\Services\Forms\Fields\Input;
+use A17\Twill\Services\Forms\Fields\Medias;
+use A17\Twill\Services\Forms\Fields\Select;
 use A17\Twill\View\Components\Blocks\TwillBlockComponent;
+use App\Services\Twill\Forms\CollapsibleContainer;
 use Illuminate\Contracts\View\View;
 
 class Inlinebannerwithglasscontainer extends TwillBlockComponent
@@ -18,8 +22,86 @@ class Inlinebannerwithglasscontainer extends TwillBlockComponent
     public function getForm(): Form
     {
         return Form::make([
-            Input::make()->name('title'),
-            Wysiwyg::make()->name('text')
+
+            Wysiwyg::make()
+                ->name('content')
+                ->label('Content')
+
+                ->toolbarOptions(
+                    [
+                        ['header' => [2, 3, 4, 5, 6, false]],
+                        'bold',
+                        'clean',
+                    ]
+                )
+                ->translatable(),
+            CollapsibleContainer::make()
+                ->label('Background Images')
+                ->fields([
+                    Medias::make()
+                        ->name('desktop_image')
+                        ->label('Background Image')
+                        ->max(1)
+                        ->fieldNote('This image will be displayed behind the content in desktop devices.'),
+                    Medias::make()
+                        ->name('tablet_image')
+                        ->label('Background Image')
+                        ->max(1)
+                        ->fieldNote('This image will be displayed behind the content in tablet devices.'),
+                    Medias::make()
+                        ->name('mobile_image')
+                        ->label('Background Image')
+                        ->max(1)
+                        ->fieldNote('This image will be displayed behind the content in mobile devices.'),
+                ]),
+            CollapsibleContainer::make()
+                ->label('Link (Button)')
+                ->fields([
+                    Input::make()
+                        ->name('link_text')
+                        ->label('Link Text')
+                        ->translatable(),
+
+                    Input::make()
+                        ->type('url')
+                        ->name('link_url')
+                        ->label('Link URL')
+                        ->translatable(),
+                ]),
         ]);
+    }
+    public static function getCrops(): array
+    {
+        return [
+            'desktop_image' => [
+                'default' => [
+                    [
+                        'name' => 'default',
+                        'ratio' => 0,
+                    ],
+                ],
+            ],
+            'tablet_image' => [
+                'default' => [
+                    [
+                        'name' => 'default',
+                        'ratio' => 0,
+                    ],
+                ],
+            ],
+            'mobile_image' => [
+                'default' => [
+                    [
+                        'name' => 'default',
+                        'ratio' => 0,
+                    ],
+                ],
+            ]
+        ];
+    }
+
+    public static function getBlockTitle(?Block $block = null): string
+    {
+        return 'Inline Banner with Glass Container';
     }
 }
--- resources/views/components/twill/blocks/inlinebannerwithglasscontainer.blade.php
+++ resources/views/components/twill/blocks/inlinebannerwithglasscontainer.blade.php
@@ -1,4 +1,27 @@
-<div>
-  <h2>{{ $input('title') }}</h2>
-  {!! $input('text') !!}
-</div>
+@php
+    $has_desktop_image = $block->hasImage('desktop_image');
+    $has_tablet_image = $block->hasImage('tablet_image');
+    $has_mobile_image = $block->hasImage('mobile_image');
+@endphp
+<section class="banner glass"
+    style="
+        @if($has_desktop_image)
+            {{ '--desktop-image: url(' . $block->image('desktop_image') . ');' }}
+        @endif
+        @if($has_tablet_image)
+            {{ '--tablet-image: url(' . $block->image('tablet_image') . ');' }}
+        @endif
+        @if($has_mobile_image)
+            {{ '--mobile-image: url(' . $block->image('mobile_image') . ');' }}
+        @endif
+    "
+    >
+    <div class="container xlarge">
+        <div class="flex-column justify-content-center align-items-center">
+            <div class="window">
+                {!! $block->translatedInput('content') !!}
+                <a href="{{$block->translatedInput('link_url')}}" class="primary-cta">{{$block->translatedInput('link_text')}}</a>
+            </div>
+        </div>
+    </div>
+</section>
